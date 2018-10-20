e2f4ff849cdcfb759c5b57cc44d32c09aea715da
gangadharyelwande <gangadhar.yelwande@capgemini.com>
2018-10-20 11:42:05 -0400

    initial commit

---
+++ limits-service/src/main/java/com/poc/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,29 @@
+package com.poc.microservices.limitsservice;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.poc.microservices.limitsservice.bean.LimitConfiguration;
+
+
+@RestController
+public class LimitsConfigurationController {
+
+
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		//return new LimitConfiguration(1000,1);
+		
+		LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMaximum(), 
+				configuration.getMinimum());
+		return limitConfiguration;
+
+	}
+	
+	
+}
