d4fc22222235a3adb7ca1633b542ee079f302d51
gangadharyelwande <gangadhar.yelwande@capgemini.com>
2018-10-20 19:37:43 -0400

    added zipkin tracing server for all services

--- currency-exchange-service/src/main/java/com/poc/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/poc/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.poc.microservices.currencyexchangeservice;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 	
-	//private Logger logger = LoggerFactory.getLogger(this.getClass());
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private Environment environment;
@@ -32,7 +34,7 @@ public class CurrencyExchangeController {
 		exchangeValue.setPort(
 				Integer.parseInt(environment.getProperty("local.server.port")));
 		
-		//logger.info("{}", exchangeValue);
+		logger.info("{}", exchangeValue);
 		
 		return exchangeValue;
 		
